d2b94fa613164276a4000fdf5ee6c4f7b279b050
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-24 14:00:56 -0700

    Fix annoying duplicate faker name issue

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
+  sequence :name do |n|
+   Faker::Space.constellation + n.to_s
+  end
+
+  sequence :description do |n|
+    Faker::Space.star + n.to_s
+  end
+  
   factory :item do
-    name { Faker::Space.constellation }
+    name
     description { Faker::Space.star }
     unit_price 1000
   end
