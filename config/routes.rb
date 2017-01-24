8a2a4575a5412e3e57f73b93a5abd2219366fac0
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-24 15:26:45 -0700

    Passing tests for show_index_random invoices

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
         get 'random', to: 'random#show'
       end
       resources 'items', only: [:index, :show]
+      namespace 'invoices' do
+        get 'random', to: 'random#show'
+      end
+      resources 'invoices', only: [:index, :show]
     end
   end
 end
