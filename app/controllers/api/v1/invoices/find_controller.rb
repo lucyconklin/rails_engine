ad5f04e833d698b6e7adc026ef2d70be175d1d63
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-24 15:43:56 -0700

    Passing tests for invoice find all

--- app/controllers/api/v1/invoices/find_controller.rb
+++ app/controllers/api/v1/invoices/find_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::Invoices::FindController < ApplicationController
     render json: Invoice.where(find_params).first
   end
 
+  def index
+    render json: Invoice.where(find_params)
+  end
+
   private
 
   def find_params
