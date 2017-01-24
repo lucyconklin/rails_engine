e22904670eabb95a9da9e1d3bc01aa9d4704d591
Brett Rivers <riverswb@gmail.com>
2017-01-24 06:04:01 -0700

    Adds strong params to merchants find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::Merchants::FindController < ApplicationController
 
   def show
-    render json: Merchant.find_by("lower(name) LIKE lower(?)", "%#{params[:name]}%")
+    render json: Merchant.find_by("lower(name) LIKE lower(?)", "%#{find_params[:name]}%")
+  end
+
+private
+
+  def find_params
+    params.permit(:name)
   end
 end
