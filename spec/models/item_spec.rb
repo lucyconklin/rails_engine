b9a7aaed354a1d0967e585bbd5edd5ef2c1d4be3
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-23 20:58:37 -0700

    Add model foreign key and relationships for items

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:unit_price) }
+  end
+
+  describe "relationships" do
+    it { should belong_to(:merchant) }
+  end
+end
