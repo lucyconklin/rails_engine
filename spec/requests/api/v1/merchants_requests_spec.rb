e05b89943ac3bb1a88cc94f759d8e98d3cb64ce8
Brett Rivers <riverswb@gmail.com>
2017-01-23 20:05:19 -0700

    Adds more testing to be sure it finds the correct merchant

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -19,20 +19,39 @@ describe "Merchants API" do
     expect(merchant["name"]).to be_a(String)
   end
 
-  it "returns a merchant" do
-    create(:merchant)
+  it "returns a specific merchant" do
+    merchants = create_list(:merchant, 4)
+    first_merchant = merchants.first
+    last_merchant = merchants.last
 
     get '/api/v1/merchants/1'
 
-    merchant = JSON.parse(response.body)
+    merchant_1 = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchant).to be_a(Hash)
-    expect(merchant).to have_key("id")
-    expect(merchant).to have_key("name")
-    expect(merchant).to have_key("created_at")
-    expect(merchant).to have_key("updated_at")
-    expect(merchant["id"]).to be_a(Integer)
-    expect(merchant["name"]).to be_a(String)
+    expect(merchant_1).to be_a(Hash)
+    expect(merchant_1).to have_key("id")
+    expect(merchant_1).to have_key("name")
+    expect(merchant_1).to have_key("created_at")
+    expect(merchant_1).to have_key("updated_at")
+    expect(merchant_1["id"]).to be_a(Integer)
+    expect(merchant_1["id"]).to eq(first_merchant["id"])
+    expect(merchant_1["name"]).to be_a(String)
+    expect(merchant_1["name"]).to eq(first_merchant["name"])
+
+    get '/api/v1/merchants/4'
+
+    merchant_4 = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant_4).to be_a(Hash)
+    expect(merchant_4).to have_key("id")
+    expect(merchant_4).to have_key("name")
+    expect(merchant_4).to have_key("created_at")
+    expect(merchant_4).to have_key("updated_at")
+    expect(merchant_4["id"]).to be_a(Integer)
+    expect(merchant_4["id"]).to eq(last_merchant["id"])
+    expect(merchant_4["name"]).to be_a(String)
+    expect(merchant_4["name"]).to eq(last_merchant["name"])
   end
 end
