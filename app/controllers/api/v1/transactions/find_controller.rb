abb8ce25fa6be1e9bd9eebaf31c9dead09e4bae3
Brett Rivers <riverswb@gmail.com>
2017-01-24 16:01:55 -0700

    Adds tests for finding single transaction when searching by any param

---
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Transactions::FindController < ApplicationController
+
+  def show
+    render json: Transaction.find_by(find_params)
+  end
+
+  private
+
+  def find_params
+    params.permit(:id, :credit_card_number, :credit_card_expiration_date, :created_at, :updated_at)
+  end
+end
--- spec/requests/api/v1/transactions/trasaction_requests_find_spec.rb
+++ spec/requests/api/v1/transactions/trasaction_requests_find_spec.rb
@@ -16,4 +16,45 @@ describe 'Transactions Single Finder API' do
     expect(transaction).to be_a(Hash)
     expect(transaction["credit_card_number"]).to eq(@transaction_1.credit_card_number)
   end
+
+  it 'finds transaction by result' do
+    get "/api/v1/transactions/find?result=#{@transaction_1.result}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction).to be_a(Hash)
+    expect(transaction["result"]).to eq(@transaction_1.result)
+  end
+
+  it 'finds transaction by credit_card_number' do
+    get "/api/v1/transactions/find?credit_card_number=#{@transaction_1.credit_card_number}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction).to be_a(Hash)
+    expect(transaction["credit_card_number"]).to eq(@transaction_1.credit_card_number)
+  end
+
+  it 'finds transaction by created_at' do
+    get "/api/v1/transactions/find?created_at=#{@transaction_1.created_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction).to be_a(Hash)
+    expect(transaction["credit_card_number"]).to eq(@transaction_1.credit_card_number)
+  end
+
+  it 'finds transaction by updated at' do
+    get "/api/v1/transactions/find?updated_at=#{@transaction_1.updated_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction).to be_a(Hash)
+    expect(transaction["credit_card_number"]).to eq(@transaction_1.credit_card_number)
+  end
+
 end
