219f1d79445f3864a91d5864e3d8acc6efa25565
Brett Rivers <riverswb@gmail.com>
2017-01-25 15:44:54 -0700

    Adds test for items/:id/merchant

--- spec/requests/api/v1/relationships/items/items_invoice_items_spec.rb
+++ spec/requests/api/v1/relationships/items/items_invoice_items_spec.rb
@@ -4,15 +4,25 @@ describe 'Items invoice items relationship' do
   it 'returns collection of associated invoice items' do
     item = create(:item)
     create_list(:invoice_item, 100, item_id: item.id)
-    create_list(:invoice_item, 100)
 
     get "/api/v1/items/#{item.id}/invoice_items"
 
     found = JSON.parse(response.body)
 
-    expect(InvoiceItem.count).to eq(200)
     expect(found.count).to eq(100)
     expect(found).to be_a(Array)
     expect(found.first["id"]).to eq(item.id)
+  end
+
+  it 'returns merchant associated with an item' do
+    create_list(:item, 20)
+    item = Item.all.sample
+
+    get "/api/v1/items/#{item.id}/merchant"
+
+    found = JSON.parse(found.body)
+
+    expect(found).to be_a(Hash)
+    expect(found.id).to eq(item.merchant_id)
   end
 end
