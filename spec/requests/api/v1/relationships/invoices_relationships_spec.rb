650ab89b929c4f603ec99af52b43b16ebc8b92bc
Brett Rivers <riverswb@gmail.com>
2017-01-25 16:14:02 -0700

    Adds test for finding transactions by invoice id

---
+++ spec/requests/api/v1/relationships/invoices_relationships_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Invoices Relationship Requests' do
+  it 'returns collection of transactions by invoice id' do
+    invoice = create(:invoice)
+    create_list(:transaction, 5, invoice_id: invoice.id)
+    create_list(:transaction, 5)
+
+    get "/api/v1/invoices/#{invoice.id}/transactions"
+
+    found = JSON.parse(rsponse.body)
+
+    expect(Transaction.count).to eq(10)
+    expect(found).to be_a(Hash)
+    expect(found.count).to eq(5)
+    expect(found.first.invoice_id).to eq(invoice.id)
+  end
+end
