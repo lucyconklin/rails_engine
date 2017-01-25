574838b5bdd4db08010d53a1673b9df5d9ab4a25
Brett Rivers <riverswb@gmail.com>
2017-01-25 16:29:34 -0700

    Adds test for invoices/:id/invoice_items

--- spec/requests/api/v1/relationships/invoices_relationships_spec.rb
+++ spec/requests/api/v1/relationships/invoices_relationships_spec.rb
@@ -15,4 +15,20 @@ describe 'Invoices Relationship Requests' do
     expect(found.count).to eq(5)
     expect(found.first["invoice_id"]).to eq(invoice.id)
   end
+
+  it 'returns a collection of invoice items by invoice id' do
+    create_list(:invoice, 5)
+    invoice = Invoice.all.sample
+    create_list(:invoice_item, 5, invoice_id: invoice.id)
+    create_list(:invoice_item, 5)
+
+    get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+    found = JSON.parse(response.body)
+
+    expect(InvoiceItem.count).to eq(10)
+    expect(found).to be_a(Array)
+    expect(found.count).to eq(5)
+    expect(found.first["invoice_id"]).to eq(invoice.id)
+  end
 end
