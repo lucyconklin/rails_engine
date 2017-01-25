09a2e8c9c84f3d1d4a7c4865bff8cbd293e7c291
Brett Rivers <riverswb@gmail.com>
2017-01-24 20:08:05 -0700

    Adds testing for all find params for customers

--- spec/requests/api/v1/customers/customers_find_request_spec.rb
+++ spec/requests/api/v1/customers/customers_find_request_spec.rb
@@ -16,4 +16,49 @@ describe 'Customers Single Finder API' do
     expect(customer["first_name"]).to eq(@customer_1.first_name)
     expect(customer["last_name"]).to eq(@customer_1.last_name)
   end
+
+  it 'finds customer by first name' do
+    get "/api/v1/customers/find?first_name=#{@customer_1.first_name}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer).to be_a(Hash)
+    expect(customer["first_name"]).to eq(@customer_1.first_name)
+    expect(customer["last_name"]).to eq(@customer_1.last_name)
+  end
+
+  it 'finds customer by last_name' do
+    get "/api/v1/customers/find?last_name=#{@customer_1.last_name}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer).to be_a(Hash)
+    expect(customer["first_name"]).to eq(@customer_1.first_name)
+    expect(customer["last_name"]).to eq(@customer_1.last_name)
+  end
+
+  it 'finds customer by created_at' do
+    get "/api/v1/customers/find?created_at=#{@customer_1.created_at}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer).to be_a(Hash)
+    expect(customer["first_name"]).to eq(@customer_1.first_name)
+    expect(customer["last_name"]).to eq(@customer_1.last_name)
+  end
+
+  it 'finds customer by updated_at' do
+    get "/api/v1/customers/find?updated_at=#{@customer_1.updated_at}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer).to be_a(Hash)
+    expect(customer["first_name"]).to eq(@customer_1.first_name)
+    expect(customer["last_name"]).to eq(@customer_1.last_name)
+  end
+
 end
