121b18d74cdfd4ce485b8c756f24976b7910f7e5
Brett Rivers <riverswb@gmail.com>
2017-01-25 15:21:33 -0700

    Adds more invoice items to ensure items/:id/invoice_items isn't returning all.

--- spec/requests/api/v1/relationships/items/items_invoice_items_spec.rb
+++ spec/requests/api/v1/relationships/items/items_invoice_items_spec.rb
@@ -4,11 +4,13 @@ describe 'Items invoice items relationship' do
   it 'returns collection of associated invoice items' do
     item = create(:item)
     create_list(:invoice_item, 100, item_id: item.id)
+    create_list(:invoice_item, 100)
 
     get "/api/v1/items/#{item.id}/invoice_items"
 
     found = JSON.parse(response.body)
 
+    expect(InvoiceItem.count).to eq(200)
     expect(found.count).to eq(100)
     expect(found).to be_a(Array)
     expect(found.first["id"]).to eq(item.id)
