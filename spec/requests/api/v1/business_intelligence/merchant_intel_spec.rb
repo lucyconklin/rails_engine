d4d3e03e84e208ea05f87020c2adf95c46ecafff
Brett Rivers <riverswb@gmail.com>
2017-01-26 12:17:15 -0700

    Adds test for revenue from date for all merchants

--- spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
@@ -28,3 +28,21 @@ describe 'Single Merchant Business Intelligence' do
     expect(found["revenue"]).to be_a(String)
   end
 end
+
+describe "All Merchants Business Intelligence" do
+  before(:each) do
+    @invoice = create(:invoice)
+    invoice_items = create_list(:invoice_item, 3, invoice_id: @invoice.id)
+    transactions = create_list(:transaction, 3, invoice_id: @invoice.id)
+  end
+
+  it "returns total revenue for date accross all merchants" do
+    get "/api/v1/merchants/revenue?date=#{@invoice.created_at}"
+
+    found = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(found).to have_key("revenue")
+    expect(found["revenue"]).to be_a(String)
+  end
+end
