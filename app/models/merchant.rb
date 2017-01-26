ce2929c69e64a51a1e87da78e25db27f5e98f883
Brett Rivers <riverswb@gmail.com>
2017-01-26 11:30:53 -0700

    Refactors merchant revenue controller to use total_revenue in merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,10 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+
+  def total_revenue
+    invoices.joins(:transactions, :invoice_items)
+    .merge(Transaction.success)
+    .sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
