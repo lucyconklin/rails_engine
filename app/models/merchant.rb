d477b982140612060c3620c03f935edc0de4f4fd
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-26 10:59:27 -0700

    merchant favorite customer

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,18 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
   has_many :invoices
+  has_many :customers, through: :invoices
+
+  def favorite_customer
+    customers.joins(:transactions)
+            .merge(Transaction.success)
+            .group("customers.id")
+            .order("count(transactions.id) DESC")
+            .first
+  end
+
+  def self.most_revenue(quantity = 1)
+    joins(:invoices)
+    # .merge(Transaction.success)
+  end
 end
