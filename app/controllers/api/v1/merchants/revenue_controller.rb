1213427b8e17e92f0ffca828630daf09a2f93cf3
Brett Rivers <riverswb@gmail.com>
2017-01-26 11:25:55 -0700

    Refactors merchant revenue controller to use transaction success

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,9 +3,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def show
      render json: Merchant.find(params[:id])
      .invoices.joins(:transactions, :invoice_items)
-     .where(transactions: {result: "success"})
+     .merge(Transaction.success)
      .sum("invoice_items.unit_price * invoice_items.quantity"),
      serializer: SingleMerchantSerializer
-
   end
 end
