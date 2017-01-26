5df5b60fd07918e8b143d3ffc6b1acc956b40a44
Brett Rivers <riverswb@gmail.com>
2017-01-26 11:30:53 -0700

    Refactors merchant revenue controller to use total_revenue in merchant model

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,10 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-     render json: Merchant.find(params[:id])
-     .invoices.joins(:transactions, :invoice_items)
-     .merge(Transaction.success)
-     .sum("invoice_items.unit_price * invoice_items.quantity"),
+     render json: Merchant.find(params[:id]).total_revenue,
      serializer: SingleMerchantSerializer
   end
 end
