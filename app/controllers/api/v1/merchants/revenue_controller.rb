38dd49c4c44bc8391d77371edb15ce2ae1fb595e
Brett Rivers <riverswb@gmail.com>
2017-01-26 11:05:46 -0700

    Adds to merchant revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-     merchant = Merchant.find(params[:id])
-     merchant.invoices.joins(:transactions, :invoice_items).where(transactions: {result: "success"}) .sum("invoice_items.unit_price * invoice_items.quantity)")
+     render json: Merchant.find(params[:id])
+     .invoices.joins(:transactions, :invoice_items)
+     .where(transactions: {result: "success"})
+     .sum("invoice_items.unit_price * invoice_items.quantity"),
+     serializer: SingleMerchantSerializer
+
   end
 end
--- spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_intel_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'Single Merchant Business Intelligence' do
   before(:each) do
     invoice = create(:invoice)
-    invoice_items = create_list(:invoice_item, 10, invoice_id: invoice.id)
-    transactions = create_list(:transaction, 10, invoice_id: invoice.id)
+    invoice_items = create_list(:invoice_item, 3, invoice_id: invoice.id)
+    transactions = create_list(:transaction, 3, invoice_id: invoice.id)
   end
 
   it 'returns total revenue accross all transactions' do
@@ -12,5 +12,10 @@ describe 'Single Merchant Business Intelligence' do
 
     get "/api/v1/merchants/#{merchant.id}/revenue"
 
+    found = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(found).to have_key("revenue")
+    expect(found["revenue"]).to be_a(String)
   end
 end
