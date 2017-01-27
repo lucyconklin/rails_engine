c1b51e0f7a7664c42d6af90f2762c434cb38df2f
Brett Rivers <riverswb@gmail.com>
2017-01-26 17:17:42 -0700

    Refactors best day controller to use best day method in item model

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Items::BestDayController < ApplicationController
 
   def show
-    render json: Item.find(params[:id]).invoices.joins(:invoice_items).group("invoices.id").order("sum(invoice_items.quantity) DESC").first.created_at,
+    render json: Item.find(params[:id]).best_day,
     serializer: ItemBestDaySerializer
   end
 end
