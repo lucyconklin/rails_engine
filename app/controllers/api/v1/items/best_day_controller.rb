d173d68b4e6486d456b0ea5b1bb846b1b5c9ecfd
Brett Rivers <riverswb@gmail.com>
2017-01-26 17:06:10 -0700

    Adds show to controller, works in console

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Items::BestDayController < ApplicationController
 
   def show
-    
+    render json: Item.find(params[:id]).invoices.joins(:invoice_items).group("invoices.id").order("sum(invoice_items.quantity) DESC").first.created_at,
+    serializer: 
   end
 end
