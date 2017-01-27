a49a25e794ca74470187fe83979e722c00dd56e4
Lucy Conklin <lucy.conklin@gmail.com>
2017-01-26 17:14:32 -0700

    Passing tests for merchant top by items

--- spec/requests/api/v1/business_intelligence/merchant_most_items_sold_spec.rb
+++ spec/requests/api/v1/business_intelligence/merchant_most_items_sold_spec.rb
@@ -4,36 +4,38 @@ describe "Merchant Business Intelligence most sold" do
   before(:each) do
     create_list(:merchant, 4)
     @merchant = Merchant.last
-    # invoice_1 = create(:invoice, merchant_id: @merchant.id)
-    # invoice_2 = create(:invoice, merchant_id: @merchant.id)
-    # invoice_item = create(:invoice_item, invoice_id: invoice_1.id, item_id: @merchant.items.first.id, quantity: 100, unit_price: 1000)
-    # invoice_item = create(:invoice_item, invoice_id: invoice_1.id, item_id:@merchant.items.last.id, quantity: 100, unit_price: 1000)
-    # invoice_item = create(:invoice_item, invoice_id: invoice_2.id, item_id: Merchant.last.items.first.id, quantity: 1, unit_price: 10)
-    # create(:transaction, invoice_id: invoice_1.id, result: "success")
-    # create(:transaction, invoice_id: invoice_2.id, result: "success")
+    merchant_2 = create(:merchant)
+    create_list(:item, 2, merchant_id: merchant_2.id)
+    create(:item, merchant_id: @merchant.id)
+    invoice_1 = create(:invoice, merchant_id: @merchant.id)
+    invoice_2 = create(:invoice, merchant_id: merchant_2.id)
+    invoice_item = create(:invoice_item, invoice_id: invoice_1.id, item_id: @merchant.items.first.id, quantity: 10, unit_price: 1000)
+    invoice_item = create(:invoice_item, invoice_id: invoice_1.id, item_id:@merchant.items.last.id, quantity: 10, unit_price: 1000)
+    invoice_item = create(:invoice_item, invoice_id: invoice_2.id, item_id: merchant_2.items.first.id, quantity: 10, unit_price: 10)
+    create(:transaction, invoice_id: invoice_1.id, result: "success")
+    create(:transaction, invoice_id: invoice_2.id, result: "success")
   end
 
   it "returns a merchant ranked by total items sold" do
-  #   get "/api/v1/merchants/most_revenue"
-  #
-  #   merchants = JSON.parse(response.body)
-  #   merchant = merchants.first
-  #
+    get "/api/v1/merchants/most_items"
+
+    merchants = JSON.parse(response.body)
+    merchant = merchants.first
+
     expect(response).to be_success
-  #   expect(merchants.count).to eq(1)
-  #   expect(merchant["id"]).to eq(@merchant.id)
-  #   expect(merchant["name"]).to eq(@merchant.name)
+    expect(merchant["id"]).to eq(@merchant.id)
+    expect(merchant["name"]).to eq(@merchant.name)
   end
   #
   it "returns x merchants ranked by total items sold" do
-  #   get "/api/v1/merchants/most_revenue?quantity=2"
-  #
-  #   merchants = JSON.parse(response.body)
-  #   merchant = merchants.first
-  #
+    get "/api/v1/merchants/most_items?quantity=2"
+
+    merchants = JSON.parse(response.body)
+    merchant = merchants.first
+
     expect(response).to be_success
-  #   expect(merchants.count).to eq(2)
-  #   expect(merchant["id"]).to eq(@merchant.id)
-  #   expect(merchant["name"]).to eq(@merchant.name)
+    expect(merchants.count).to eq(2)
+    expect(merchant["id"]).to eq(@merchant.id)
+    expect(merchant["name"]).to eq(@merchant.name)
   end
 end
